744a6eedb02426561fa9ec55f05b6cf29387e98b
Alec <horwitz_alec@wheatoncollege.edu>
2019-05-09 16:45:57 -0400

    determined a way to keep secrets from git repo without .env files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import API_KEY from './.env'
+import {API_KEY} from './Secrets.js'
 
 class App extends Component {
   state = {data: null}
 
   componentDidMount = () => {
-    fetch(`https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=${API_KEY}`)
+    console.log(API_KEY)
+    fetch("https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key="+API_KEY)
     .then(data => data.json())
     .then(data => this.setState({data}))
   }
